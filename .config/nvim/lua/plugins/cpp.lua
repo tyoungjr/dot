73c86b96a4dfd2da7f5606e7938483726e56957e
teej <tyoungjr2005@gmail.com>
2025-11-12 13:14:51 -0800

    nvim: add C++ plugin config with clangd, cmake-tools, and debugging

---
+++ .config/nvim/lua/plugins/cpp.lua
@@ -0,0 +1,119 @@
+return {
+  -- Treesitter for C++
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "cpp",
+        "c",
+      })
+    end,
+  },
+
+  -- LSP for C++ (clangd)
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        clangd = {
+          cmd = {
+            "clangd",
+            "--background-index",
+            "--clang-tidy",
+            "--header-insertion=iwyu",
+            "--completion-style=detailed",
+            "--function-arg-placeholders",
+            "--fallback-style=llvm",
+          },
+          init_options = {
+            usePlaceholders = true,
+            completeUnimported = true,
+            clangdFileStatus = true,
+          },
+          root_dir = function(fname)
+            return require("lspconfig.util").root_pattern(
+              "CMakeLists.txt",
+              "CMakePresets.json",
+              "compile_commands.json",
+              ".git"
+            )(fname)
+          end,
+        },
+      },
+    },
+  },
+
+  -- CMake integration
+  {
+    "Civitasv/cmake-tools.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    opts = {
+      cmake_command = "cmake",
+      cmake_build_directory = "build",
+      cmake_generate_options = { "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON" },
+      cmake_build_options = {},
+      cmake_console_size = 10,
+      cmake_show_console = "always",
+      cmake_dap_configuration = {
+        name = "cpp",
+        type = "codelldb",
+        request = "launch",
+        stopOnEntry = false,
+        runInTerminal = true,
+        console = "integratedTerminal",
+      },
+      cmake_variants_message = {
+        short = { show = true },
+        long = { show = true, max_length = 40 },
+      },
+    },
+  },
+
+  -- Debugging with DAP
+  {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+      "rcarriga/nvim-dap-ui",
+      "theHamsta/nvim-dap-virtual-text",
+    },
+    config = function()
+      local dap = require("dap")
+
+      -- C++ adapter (using codelldb)
+      dap.adapters.codelldb = {
+        type = "server",
+        port = "${port}",
+        executable = {
+          command = vim.fn.exepath("codelldb"),
+          args = { "--port", "${port}" },
+        },
+      }
+
+      dap.configurations.cpp = {
+        {
+          name = "Launch Unity Plugin",
+          type = "codelldb",
+          request = "launch",
+          program = function()
+            return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+          end,
+          cwd = "${workspaceFolder}",
+          stopOnEntry = false,
+        },
+      }
+    end,
+  },
+
+  -- C++ formatting with clang-format
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        cpp = { "clang_format" },
+        c = { "clang_format" },
+      },
+    },
+  },
+}
