dc2d55b7455849e4152d9c268240d544de85e697
teej <tyoungjr2005@gmail.com>
2025-11-12 14:13:52 -0800

    nvim: add CMake plugin config with cmake LSP and cmake_format formatting

---
+++ .config/nvim/lua/plugins/cmake.lua
@@ -0,0 +1,37 @@
+return {
+  -- Treesitter for CMake
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, { "cmake" })
+    end,
+  },
+
+  -- LSP for CMake
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        cmake = {
+          root_dir = function(fname)
+            return require("lspconfig.util").root_pattern(
+              "CMakePresets.json",
+              "CMakeLists.txt",
+              ".git"
+            )(fname)
+          end,
+        },
+      },
+    },
+  },
+
+  -- CMake formatting
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        cmake = { "cmake_format" },
+      },
+    },
+  },
+}
