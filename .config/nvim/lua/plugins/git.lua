2110ae24706d5efc0e61085b10262cbc51bc27e5
teej <tyoungjr2005@gmail.com>
2025-11-12 14:14:30 -0800

    nvim: add Git plugin config with gitsigns, git-lfs, and lazygit integration

---
+++ .config/nvim/lua/plugins/git.lua
@@ -0,0 +1,46 @@
+return {
+  -- Git integration
+  {
+    "lewis6991/gitsigns.nvim",
+    opts = {
+      signs = {
+        add = { text = "▎" },
+        change = { text = "▎" },
+        delete = { text = "" },
+        topdelete = { text = "" },
+        changedelete = { text = "▎" },
+        untracked = { text = "▎" },
+      },
+      current_line_blame = true,
+      current_line_blame_opts = {
+        virt_text = true,
+        virt_text_pos = "eol",
+        delay = 300,
+      },
+    },
+  },
+
+  -- Git-LFS support
+  {
+    "lewis6991/gitsigns.nvim",
+    init = function()
+      vim.api.nvim_create_autocmd("VimEnter", {
+        callback = function()
+          -- Ensure git-lfs is initialized
+          vim.fn.system("git lfs install")
+        end,
+      })
+    end,
+  },
+
+  -- Lazygit integration
+  {
+    "kdheepak/lazygit.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    keys = {
+      { "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+    },
+  },
+}
