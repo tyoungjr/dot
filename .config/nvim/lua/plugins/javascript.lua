7afbe88fa45dbc291e41d568375095c719cea48f
teej <tyoungjr2005@gmail.com>
2025-11-12 14:12:32 -0800

    nvim: add JavaScript plugin config with ts_ls LSP and prettier formatting

---
+++ .config/nvim/lua/plugins/javascript.lua
@@ -0,0 +1,41 @@
+return {
+  -- Treesitter for JavaScript
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "javascript",
+        "json",
+      })
+    end,
+  },
+
+  -- LSP for JavaScript (tsserver)
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        ts_ls = {
+          root_dir = function(fname)
+            return require("lspconfig.util").root_pattern(
+              "package.json",
+              "package-lock.json",
+              ".git"
+            )(fname)
+          end,
+        },
+      },
+    },
+  },
+
+  -- Formatting for JavaScript
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        javascript = { "prettier" },
+        json = { "prettier" },
+      },
+    },
+  },
+}
